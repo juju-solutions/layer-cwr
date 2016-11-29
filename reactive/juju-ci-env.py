79b718c73878f0e7242b98fecc89cbbc4bc7b983
Konstantinos Tsakalozos <kos.tsakalozos@canonical.com>
2016-11-29 16:57:51 +0200

    Use extension interface to connect to Jenkins. Use rest interface to interact with Jenkins

--- reactive/juju-ci-env.py
+++ reactive/juju-ci-env.py
@@ -1,7 +1,10 @@
+import os
 from charmhelpers import fetch
 from charmhelpers.core import host, hookenv
 from charms.reactive import when, when_not, set_state, remove_state
 from jujubigdata import utils
+from jenkins import Jenkins, JenkinsException
+
 
 @when_not('juju-ci-env.installed')
 def install_juju():
@@ -11,7 +14,6 @@ def install_juju():
     fetch.apt_install(["juju", "zfsutils-linux", "charm-tools", "unzip", "expect"])
 
     utils.run_as('root', 'lxd', 'init', '--auto')
-
     utils.run_as('root', 'scripts/lxd-reconf.sh')
 
     # Make user jenkins parametrised. And this action as well
@@ -22,7 +24,32 @@ def install_juju():
     utils.run_as('root', 'python', '-m', 'pip', 'install', 'bundletester')
     utils.run_as('root', 'python', '-m', 'pip', 'install', 'cloud-weather-report')
 
-    utils.run_as('jenkins', 'cp', '-r', 'jobs', '/var/lib/jenkins')
     set_state('juju-ci-env.installed')
     hookenv.status_set('active', 'Ready')
 
+
+@when('jenkins.available')
+@when_not('jenkins.jobs.ready')
+def install_jenkins_jobs(connected_jenkins):
+    hookenv.status_set('maintenance', 'Uploading Jenkins jobs')
+    jenkins_connection_info = connected_jenkins.get_connection_info()
+    jclient = Jenkins(jenkins_connection_info["jenkins_url"],
+                      jenkins_connection_info["admin_username"],
+                      jenkins_connection_info["admin_password"])
+
+    for dirname, dirnames, _ in os.walk('jobs'):
+        for subdirname in dirnames:
+            jobfilename = os.path.join(dirname, subdirname, "config.xml")
+            with open(jobfilename, 'r') as jobfile:
+                configxml = jobfile.read()
+                jclient.create_job(subdirname, configxml)
+
+    plugins = ["github", "ghprb", "postbuildscript"]
+    for plugin in plugins:
+        reboot = jclient.install_plugin(plugin)
+        hookenv.log("Installing plugin {}. Restart required: {}".format(plugin, reboot))
+
+    host.service_restart("jenkins")
+
+    hookenv.status_set('active', 'Ready')
+    set_state("jenkins.jobs.ready")
