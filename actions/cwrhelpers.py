d428b8398a90f6638720886a4d0d4cb7def989dd
Kevin W Monroe <kevin.monroe@canonical.com>
2016-12-13 00:04:04 +0000

    handle reference bundle better
    - rename jenkinshelpers->cwrhelpers
    - lookup app name for a given charm in a given bundle
    - use app name when deploying the local charm with a reference bundle
    - include theblues to handle charmstore interaction (for retrieving bundle.yaml)

---
+++ actions/cwrhelpers.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python3
+
+import sys
+import time
+import yaml
+
+from charmhelpers.core import hookenv
+from jenkins import NotFoundException
+from theblues import charmstore
+
+
+def app_from_bundle(bundle, charm):
+    '''Return the app name used in the given bundle for a given charm.'''
+    cs = charmstore.CharmStore()
+    bundle_yaml = cs.files(bundle, filename='bundle.yaml', read_file=True)
+    yaml_contents = yaml.safe_load(bundle_yaml)
+    for app, app_config in yaml_contents['services'].items():
+        if charm in app_config['charm']:
+            return app
+    return None
+
+
+def fail_action(msg, output=None):
+    '''Fail an action with a message and (optionally) additional output.'''
+    if output:
+        hookenv.action_set({'output': output})
+    hookenv.action_fail(msg)
+    sys.exit()
+
+
+def wait_result(jclient, job_name, build_number, secs_to_wait=60):
+    timeout = time.time() + secs_to_wait
+    while True:
+        time.sleep(5)
+        if time.time() > timeout:
+            raise Exception("Job timeout")
+        try:
+            build_info = jclient.get_build_info(job_name, build_number)
+            if build_info["result"] == 'FAILURE':
+                outcome = 'fail'
+            else:
+                outcome = 'success'
+
+            output = jclient.get_build_console_output(job_name, build_number)
+            return outcome, output
+        except NotFoundException:
+            print("Jenkins job {} not running yet".format(build_number))
+        except:
+            raise
