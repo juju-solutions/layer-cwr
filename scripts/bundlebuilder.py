6914990c7437c08a525a60c60005b45678d2e5a7
Konstantinos Tsakalozos <kos.tsakalozos@canonical.com>
2017-01-04 15:07:17 +0200

    Doc and unit tests for bunldebuilder

--- scripts/bundlebuilder.py
+++ scripts/bundlebuilder.py
@@ -2,35 +2,76 @@
 
 import sys
 import os
-import subprocess
-import shutil
-import yaml
-import re
 import hashlib
-import tempfile
-import logging
+from tempfile import mkdtemp
+from time import sleep
 from subprocess import Popen, PIPE, STDOUT
+from shutil import rmtree
+from yaml import safe_load, dump
+from re import search
+
+
+def execute(cmd, raise_exception=True):
+    """
+    Execute bash script printing the stdout and stderr without delay.
+
+    Args:
+        cmd: a list with the command
+        raise_exception: If True, will raise an exception upon a failing script
+
+    Returns: a tuple of return_code, output
+
+    """
+    print("Running {}".format(" ".join(cmd)))
+    output = ""
+    with Popen(cmd, stdout=PIPE, stderr=STDOUT, bufsize=1, universal_newlines=True) as p:
+        for line in p.stdout:
+            output += line
+            print(line, end='')
+
+        # Wait until process terminates
+        while p.poll() is None:
+            sleep(1)
+
+        if raise_exception and p.returncode != 0:
+            raise Exception("Command {} failed with {}".format(" ".join(cmd), p.returncode))
+        else:
+            return p.returncode, output
 
 
 class Bundle(object):
 
     def __init__(self, repo, branch, ci_info_file=None, BT_dry_run=False, store_push_dry_run=False):
-        self.tempdir = tempfile.mkdtemp()
-        subprocess.check_output(["git", "clone", repo, "--branch", branch, "{}/".format(self.tempdir)])
-        with open("{}/bundle.yaml".format(self.tempdir), 'r+') as stream:
-            self.bundle = yaml.safe_load(stream)
+        """
+        Grab the bundle source and initialise the object.
 
+        Args:
+            repo: repository to grab the bundle from
+            branch: branch to grab the bundle from
+            ci_info_file: override the bundle's ci-info.yaml file
+            BT_dry_run: perform a dry run on running the tests
+            store_push_dry_run: perform a dry run on pushing to the store
+        """
+        self.tempdir = mkdtemp()
+        execute(["git", "clone", repo, "--branch", branch, "{}/".format(self.tempdir)])
+        with open("{}/bundle.yaml".format(self.tempdir), 'r+') as stream:
+            self.bundle = safe_load(stream)
+
+        # Load the ci-info.yaml
         if ci_info_file:
             with open(ci_info_file, 'r+') as stream:
-                self.ci_info = yaml.safe_load(stream)
+                self.ci_info = safe_load(stream)
         elif os.path.isfile("{}/ci-info.yaml".format(self.tempdir)):
             with open("{}/ci-info.yaml".format(self.tempdir), 'r+') as stream:
-                self.ci_info = yaml.safe_load(stream)
+                self.ci_info = safe_load(stream)
         else:
             self.ci_info = dict() # or load some default ci_info
 
-        self.localtion = "cs:~{}/{}".format(self.ci_info['bundle']['namespace'], self.ci_info['bundle']['name'])
+        self.location = "cs:~{}/{}".format(self.ci_info['bundle']['namespace'], self.ci_info['bundle']['name'])
         self.upgraded = False
+
+        # We keep the sha1 digest/signature of the last bundle for
+        # which we triggered a build in the follwing file
         self.signature_file = "last_bundle.signature"
         if BT_dry_run:
             self.BT_command = ["echo", "bundletester"]
@@ -46,9 +87,15 @@ class Bundle(object):
         return self
 
     def __exit__(self, exc_type, exc_value, traceback):
-        shutil.rmtree(self.tempdir)
+        rmtree(self.tempdir)
 
     def get_charms(self):
+        """
+        Get the charms of the bundle.
+
+        Returns: the list of charm names.
+
+        """
         charms = []
         services = self.bundle['services'].values()
         for service in services:
@@ -56,24 +103,56 @@ class Bundle(object):
         return charms
 
     def get_charms_upgrade_policy(self, charm_name):
+        """
+        Scan the ci-info.yaml and return the upgrade instructions
+        for the provided charm
+        Args:
+            charm_name: name of the charm to be upgraded
+
+        Returns: dictionary with upgrade instructions or None
+
+        """
         if charm_name in self.ci_info['charm-upgrade'].keys():
             return self.ci_info['charm-upgrade'][charm_name]
         else:
             return None
 
     def upgrade(self, charm, new_revision):
+        """
+        Try to update the bundle.yaml with the revision of the charm provided.
+        If an upgrade is actually possible, the self.upgraded flag is set.
+        Args:
+            charm: the name of the charm
+            new_revision: the revision of the charm
+
+        """
         services = self.bundle['services'].values()
         for service in services:
             if service['charm'] == charm and service['charm'] != new_revision:
                 service['charm'] = new_revision
                 self.upgraded = True
                 with open("{}/bundle.yaml".format(self.tempdir), 'w') as fp:
-                    yaml.dump(self.bundle, fp)
+                    dump(self.bundle, fp)
 
     def upgradable(self):
+        """
+        Is an upgrade possible?
+
+        Returns: True if an upgrade of the bundle is possible.
+
+        """
         return self.upgraded
 
     def should_trigger_build(self):
+        """
+        Should a build be triggered?
+        To answer this we need:
+        a) to have new charms for the bundle
+        b) to make sure we have not triggered a build in the past.
+
+        Returns: True if a new build should be triggered
+
+        """
         if not self.upgradable():
             return False
 
@@ -91,8 +170,15 @@ class Bundle(object):
                 return False
 
     def get_current_signature(self):
+        """
+        Get the sha1 digest of the bundle we have so far.
+
+        Returns: sha1 digest of bundle
+
+        """
+
         with open("temp_bundle.yaml", 'w') as fp:
-            yaml.dump(self.bundle, fp)
+            dump(self.bundle, fp)
 
         sha1 = hashlib.sha1()
         with open("temp_bundle.yaml", 'rb') as f:
@@ -101,59 +187,83 @@ class Bundle(object):
         return sha1.hexdigest()
 
     def get_last_signature(self):
+        """
+        Get the digest of the bundle we last fired a build for.
+
+        Returns: string digest
+
+        """
         with open(self.signature_file, 'r') as fp:
             digest = fp.read()
         return digest
 
     def store_signature(self):
+        """
+        Store the digest of the current bundle.
+
+        Returns: string digest
+
+        """
         digest = self.get_current_signature()
         with open(self.signature_file, 'w') as f:
             f.write(digest)
         return digest
 
     def test(self, model):
+        """
+        Run tests on the bundle.
+
+        Args:
+            model: the juju model to be used for deployments.
+
+        """
         cmd = list(self.BT_command)
         cmd += ["-vFt", self.tempdir]
         cmd += ["-e", model]
         cmd += ["--bundle", "bundle.yaml"]
         cmd += ["-l", "DEBUG"]
-        self.execute(cmd)
+        execute(cmd)
 
     def release(self):
+        """
+        Push, release and grant permissions to everyone for the bundle.
+
+        Returns: True if the release process was successful
+
+        """
         if not self.ci_info['bundle']['release']:
             return False
 
         cmd = list(self.charm_command)
-        cmd += ["push", self.tempdir, self.localtion]
+        cmd += ["push", self.tempdir, self.location]
         cmd += ["--channel", self.ci_info['bundle']['to-channel']]
-        output = subprocess.check_output(cmd)
-        logging.warning("During release, calling {} with output {}".format(cmd, output))
+        execute(cmd)
 
-        output = subprocess.check_output(["charm", "show", self.localtion, "-c", self.ci_info['bundle']['to-channel'], "id"])
-        logging.warning("During release, just released {}".format(output))
-        latest = yaml.safe_load(output)
+        _, output = execute(["charm", "show", self.location, "-c", self.ci_info['bundle']['to-channel'], "id"])
+        latest = safe_load(output)
         just_released = latest['id']['Id']
 
         cmd = list(self.charm_command)
         cmd += ["grant", just_released]
         cmd += ["everyone"]
-        output = subprocess.check_output(cmd)
-        logging.warning("During release, grant to everyone with {} and output {}".format(cmd, output))
+        _, output = execute(cmd)
 
         return True
-
-    def execute(cmd):
-        with Popen(cmd, stdout=PIPE, stderr=STDOUT, bufsize=1, universal_newlines=True) as p:
-            for line in p.stdout:
-                print(line, end='')
 
 
 class Charm(object):
 
     def __init__(self, charm_name, store_push_dry_run=False):
+        """
+        Initialise this helper class.
+
+        Args:
+            charm_name: the name of the charm
+            store_push_dry_run: should we fake the push to store
+        """
         self.provided_name = charm_name
         self.name_no_namespace = charm_name[charm_name.rfind('/')+1:]
-        m = re.search(r'\-\d+$', self.name_no_namespace)
+        m = search(r'\-\d+$', self.name_no_namespace)
         # if the string ends in digits m will be a Match object, or None otherwise.
         self.name = self.name_no_namespace
         self.name_no_revision = charm_name
@@ -167,39 +277,79 @@ class Charm(object):
             self.charm_command = ["charm"]
 
     def get_latest(self, channel):
-        output = subprocess.check_output(["charm", "show", self.name_no_revision, "-c", channel, "id"])
-        latest = yaml.safe_load(output)
+        """
+        Get the latest revision of the charm present in the channel provided
+
+        Args:
+            channel: the channel to look for the charm
+
+        Returns: the latest revision of the charm
+
+        """
+        _, output = execute(["charm", "show", self.name_no_revision, "-c", channel, "id"])
+        latest = safe_load(output)
         return latest['id']['Id']
 
     def get_name(self):
+        """
+        Returns: the name of the charm
+
+        """
         return self.name
 
     def get_namespace_name(self):
+        """
+
+        Returns: the name of the charm withou the revision
+
+        """
         return self.name_no_revision
 
     def get_namespace_name_revision(self):
+        """
+        Returns: the charm as provided in this class (namespace/name-revision)
+
+        """
         return self.provided_name
 
     def release_latest(self, from_channel, to_channel):
+        """
+        Release the latest revision of the charm in the from_channel
+        to the to_channel
+
+        Args:
+            from_channel: where to get the latest charm from
+            to_channel: where to release the charm
+
+        """
         latest = self.get_latest(from_channel)
         cmd = list(self.charm_command)
         cmd += ["release", latest]
         cmd += ["--channel", to_channel]
-        subprocess.check_output(cmd)
+        execute(cmd)
         latest_just_released = self.get_latest(to_channel)
         cmd = list(self.charm_command)
         cmd += ["grant", latest_just_released]
         cmd += ["everyone"]
-        subprocess.check_output(cmd)
+        execute(cmd)
 
 
-class Tester(object):
+class Coordinator(object):
 
     def __init__(self, BT_dry_run=False, store_push_dry_run=False):
         self.BT_dry_run = BT_dry_run
         self.store_push_dry_run = store_push_dry_run
 
     def check_bundle(self, repo, branch):
+        """
+        See if the bundle needs to be updated
+        Args:
+            repo: repository to grab the bundle from
+            branch: branch  to grab the bundle from
+
+        Returns: True if a newer version of a charm is present
+
+        """
         with Bundle(repo, branch,
                     BT_dry_run=self.BT_dry_run,
                     store_push_dry_run=self.store_push_dry_run) as bundle:
@@ -231,6 +381,14 @@ class Tester(object):
                     return False
 
     def test_and_release_bundle(self, repo, branch, model):
+        """
+        Build, test and release the bundle
+        Args:
+            repo: repository to grab the bundle from
+            branch: branch  to grab the bundle from
+            model: Juju model to use for testing the charm
+
+        """
         with Bundle(repo, branch,
                     BT_dry_run=self.BT_dry_run,
                     store_push_dry_run=self.store_push_dry_run) as bundle:
@@ -265,10 +423,17 @@ class Tester(object):
 
 
 if __name__ == "__main__":
+    if "--help" in sys.argv or len(sys.argv) == 1:
+        print("Usage: {} <operation> <repo> <branch> <model>\n".format(sys.argv[0]))
+        print("  <operation>: 'check' or 'build' to check or build the bundle.")
+        print("  <repo>: repo of the bundle.")
+        print("  <branch>: branch to grab the bundle from.")
+        print("  <model>: model to be used for testing. Needed only for 'build' operation.")
+        sys.exit(1)
     operation = sys.argv[1]
     repo = sys.argv[2]
     branch = sys.argv[3]
-    tester = Tester()
+    tester = Coordinator()
     if operation == "check":
         if tester.check_bundle(repo, branch):
             sys.exit(0)
---
+++ unit_tests/test_bundlebuilder.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+
+import unittest
+from scripts import bundlebuilder
+from unittest.mock import patch, mock_open
+
+
+class TestBundlebuilder(unittest.TestCase):
+
+    bundle_yaml = "bundle:\n namespace: foo\n name: bar"
+
+    def test_execute(self):
+        retcode, output = bundlebuilder.execute(["ls", "-l"])
+        assert retcode is 0
+
+    def test_raise(self):
+        with self.assertRaises(Exception):
+            retcode, output = bundlebuilder.execute(["python3", "scripts/bundlebuilder.py", "--help"])
+
+    def test_help(self):
+        retcode, output = bundlebuilder.execute(["python3", "scripts/bundlebuilder.py", "--help"],
+                                                raise_exception=False)
+        assert retcode is 1
+        assert "Usage:" in output
+
+    @patch('scripts.bundlebuilder.execute')
+    def test_bundle(self, execute_mock):
+        with patch("builtins.open", mock_open(read_data=self.bundle_yaml)) as mock_file:
+            bundle = bundlebuilder.Bundle("http://github/myrepo", "mybranch", "myci-info.yaml",
+                                          BT_dry_run=True, store_push_dry_run=True)
+            bundle.test("mymodel")
+            assert execute_mock.call_count is 2
+
+    @patch('scripts.bundlebuilder.execute')
+    def test_charm(self, execute_mock):
+        execute_mock.return_value = (0, "id:\n Id: cs:~me/ubuntu-1")
+        charm = bundlebuilder.Charm("cs:~me/ubuntu", store_push_dry_run=True)
+        assert "ubuntu" == charm.get_name()
+        assert "cs:~me/ubuntu" == charm.get_namespace_name_revision()
+        charm.release_latest("edge", "beta")
+        print(execute_mock.call_count)
+        assert execute_mock.call_count is 4
+
+if __name__ == "__main__":
+    unittest.main()
