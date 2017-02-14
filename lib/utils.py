d03f0e277f74a1afad922a9273037961ba1e4e2c
Konstantinos Tsakalozos <kos.tsakalozos@canonical.com>
2017-02-14 12:07:43 +0200

    Improve error message when plugin installation fails

--- lib/utils.py
+++ lib/utils.py
@@ -112,7 +112,8 @@ def get_rest_path():
 def report_status():
     if is_state('jenkins.jobs.failed'):
         hookenv.status_set('blocked',
-                           'Initialisation failed. Please, redeploy cwr.')
+                           'Failed to install Jenkins plugins. '
+                           'Retry by removing/adding cwr relation.')
         return
 
     if not is_state('jenkins.available'):
