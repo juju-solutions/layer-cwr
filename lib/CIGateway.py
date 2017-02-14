8d47c5f7c2ed8cf62089830e041dadb0484d711b
Konstantinos Tsakalozos <kos.tsakalozos@canonical.com>
2017-02-14 13:32:18 +0200

    Use charmhelpers host for managing cwr service

--- lib/CIGateway.py
+++ lib/CIGateway.py
@@ -27,14 +27,16 @@ class CIGateway:
                    'charm_dir': hookenv.charm_dir()
                })
 
-        subprocess.check_call(['systemctl', 'enable', 'cwr-server'])
-        subprocess.check_call(['systemctl', 'daemon-reload'])
+        host.service_resume('cwr-server')
+        if host.init_is_systemd():
+            subprocess.check_call(['systemctl', 'daemon-reload'])
         host.service_start('cwr-server')
 
     @classmethod
     def stop(cls):
-        subprocess.check_call(['systemctl', 'disable', 'cwr-server'])
-        subprocess.check_call(['systemctl', 'daemon-reload'])
+        host.service_pause('cwr-server')
+        if host.init_is_systemd():
+            subprocess.check_call(['systemctl', 'daemon-reload'])
         host.service_stop('cwr-server')
 
     @classmethod
